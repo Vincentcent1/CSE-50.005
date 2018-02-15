d8dd6943ad08f59ecb094106c4ee39ba0dcda023
Vincentcent1 <Vincent@mymail.sutd.edu.sg>
2018-02-16 07:26:19 +0800

    fixed merge conflict

--- 2_Lab/MeanThread.java
+++ 2_Lab/MeanThread.java
@@ -1,17 +1,26 @@
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
+import java.util.stream.IntStream;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.io.File;
+import java.lang.System;
+import java.lang.Runtime;
+
 
 public class MeanThread {
 	public static void main(String[] args) throws InterruptedException, FileNotFoundException {
 		// TODO: read data from external file and store it in an array
 		       // Note: you should pass the file as a first command line argument at runtime.
 
-		int[] array = int[1048576];
-
-		Scanner sc = new Scanner(new File(args[1]));
+		int[] array = new int[1048576];
+		String[] stringArray = new String[1048576];
+		Scanner sc = new Scanner(new File(args[0]));
 
 		while(sc.hasNextLine()){
-			array = sc.nextLine().split("\\s+");
+			stringArray = sc.nextLine().trim().split("\\s+");
+			array = Arrays.stream(stringArray).mapToInt(Integer::parseInt).toArray();
+
+
 		}
 
 
@@ -19,10 +28,17 @@ public class MeanThread {
 
 
 		// define number of threads
-		int NumOfThread = Integer.valueOf(args[2]);// this way, you can pass number of threads as
+		int numOfThread = Integer.valueOf(args[1]);// this way, you can pass number of threads as
 		     // a second command line argument at runtime.
-<<<<<<< HEAD
+
+		// TODO: partition the array list into N subArrays, where N is the number of threads
+
 		int sizeOfSubarray = 1048576/numOfThread;
+		int[][] arrayPartition = new int[numOfThread][sizeOfSubarray];
+
+		for (int i = 0; i < numOfThread;i++){
+			arrayPartition[i] = Arrays.copyOfRange(array, i*sizeOfSubarray, (i+1)*sizeOfSubarray);
+		}
 
 		int processorCount= Runtime.getRuntime().availableProcessors();
 		System.out.println("Number of processors: " + processorCount);
@@ -36,62 +52,26 @@ public class MeanThread {
 
 		MeanMultiThread[] arraysOfThread = new MeanMultiThread[numOfThread];
 
-		//Use Static variable to minimize cost of copying array. Partition is also not needed with static variable
-		MeanMultiThread.initializeArray(array);
-
-
 		for (int i = 0; i < numOfThread; i++){
-			arraysOfThread[i] = new MeanMultiThread(i*sizeOfSubarray,(i + 1)*sizeOfSubarray);
+			arraysOfThread[i] = new MeanMultiThread(arrayPartition[i]);
 		}
 
 		// MeanMultiThread thread1 = new MeanMultiThread(arrayPartition[0]);
 		// MeanMultiThread threadn = new MeanMultiThread(arrayPartition[1]);
 
-=======
-
-		// TODO: partition the array list into N subArrays, where N is the number of threads
-
-		// TODO: start recording time
-
-		// TODO: create N threads and assign subArrays to the threads so that each thread computes mean of
-		    // its repective subarray. For example,
-
-		MeanMultiThread thread1 = new MeanMultiThread(subArray1);
-		MeanMultiThread threadn = new MeanMultiThread(subArrayn);
->>>>>>> parent of 630f974... MeanThread not optimised
 		//Tip: you can't create big number of threads in the above way. So, create an array list of threads.
 
 		// TODO: start each thread to execute your computeMean() function defined under the run() method
 		   //so that the N mean values can be computed. for example,
-		thread1.start(); //start thread1 on from run() function
-		threadn.start();//start thread2 on from run() function
 
-<<<<<<< HEAD
-		long startThread = System.nanoTime();
 		for (int i = 0; i < numOfThread; i++){
 			arraysOfThread[i].start();
 		}
-
 
 		for (int i = 0; i < numOfThread; i++){
 			arraysOfThread[i].join();
 		}
 
-<<<<<<< HEAD
-		long endThread = System.nanoTime();
-
-		// thread1.start(); //start thread1 on from run() function
-		// threadn.start();//start thread2 on from run() function
-
-		// thread1.join();//wait until thread1 terminates
-		// threadn.join();//wait until threadn terminates
-=======
-		thread1.join();//wait until thread1 terminates
-		threadn.join();//wait until threadn terminates
->>>>>>> parent of 630f974... MeanThread not optimised
-
-=======
->>>>>>> b6443f9
 		// TODO: show the N mean values
 		System.out.println("Temporal mean value of thread n is ... ");
 
@@ -104,9 +84,7 @@ public class MeanThread {
 		System.out.println(meanString);
 
 
-<<<<<<< HEAD
 		double globalMean = 0.0;
-
 
 		for(int i = 0; i < numOfThread; i++){
 			globalMean += arraysOfThread[i].getMean();
@@ -115,71 +93,40 @@ public class MeanThread {
 		globalMean = globalMean/numOfThread;
 
 		// TODO: stop recording time and compute the elapsed time
+
 		long finish = System.nanoTime();
 
-
 		System.out.println("Time elapsed: " + (finish-start)/1000000.0 + "ms");
-		System.out.println("Time required to start the thread: " + (endThread-startThread)/1000000.0 + "ms");
 
 		System.out.println("The global mean value is ... " + globalMean);
-=======
-		// TODO: stop recording time and compute the elapsed time
-
-		System.out.println("The global mean value is ... ");
->>>>>>> parent of 630f974... MeanThread not optimised
 
 	}
 }
 
 //Extend the Thread class
 class MeanMultiThread extends Thread {
-<<<<<<< HEAD
-	private static int[] array;
-	private int low;
-	private int high;
+	private int[] list;
 	private double mean;
 
-<<<<<<< HEAD
-	public static void initializeArray(int[] inputArray){
-		array = inputArray;
-=======
-	private ArrayList<Integer> list;
-	private double mean;
-	MeanMultiThread(ArrayList<Integer> array) {
-		list = array;
->>>>>>> parent of 630f974... MeanThread not optimised
-	}
-
-	MeanMultiThread(int low, int high) {
-		this.low = low;
-		this.high = high;
-	}
-
-=======
 	MeanMultiThread(int[] array) {
 		this.list = array;
 	}
 
->>>>>>> b6443f9
 	public double getMean() {
 		return mean;
 	}
 	public void run() {
 		// TODO: implement your actions here, e.g., computeMean(...)
-		mean = computeMean(array);
+		mean = computeMean(list);
 	}
-<<<<<<< HEAD
 
-	private double computeMean(int[] array){
+	private double computeMean(int[] list){
 		long sum = 0;
-		int localSize = high - low;
-		for (int i = low; i < high;i++){
-			sum += array[i];
+		for (int i = 0; i < list.length;i++){
+			sum += list[i];
 		}
-		double mean = (double)sum/localSize;
+		double mean = (double)sum/list.length;
 		return mean;
 	}
 
-=======
->>>>>>> parent of 630f974... MeanThread not optimised
 }
--- 2_Lab/MedianThread.java
+++ 2_Lab/MedianThread.java
@@ -1,186 +1,80 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
-import java.lang.System;
-import java.lang.Runtime;
 
 
 public class MedianThread {
 
 	public static void main(String[] args) throws InterruptedException, FileNotFoundException  {
 		// TODO: read data from external file and store it in an array
-	       // Note: you should pass the file as a first command line argument at runtime.
-		int[] array = new int[1048576];
-		String[] stringArray = new String[1048576];
-		Scanner sc = new Scanner(new File(args[0]));
-
-		while(sc.hasNextLine()){
-			stringArray = sc.nextLine().trim().split("\\s+");
-			array = Arrays.stream(stringArray).mapToInt(Integer::parseInt).toArray();
-		}
-
-
-		//TESTING
-		// int[] testArray = {7,6,5,4,3,2,0,1};
-
+	       // Note: you should pass the file as a first command line argument at runtime. 
+	
 	// define number of threads
-	int numOfThread = Integer.valueOf(args[1]);// this way, you can pass number of threads as
+	int NumOfThread = Integer.valueOf(args[1]);// this way, you can pass number of threads as 
 	     // a second command line argument at runtime.
-
+	
 	// TODO: partition the array list into N subArrays, where N is the number of threads
-
-		int sizeOfSubarray = 1048576/numOfThread;
-		int[][] arrayPartition = new int[numOfThread][sizeOfSubarray];
-
-		for (int i = 0; i < numOfThread;i++){
-			arrayPartition[i] = Arrays.copyOfRange(array, i*sizeOfSubarray, (i+1)*sizeOfSubarray);
-		}
-
-
+	
 	// TODO: start recording time
-
-		long start = System.nanoTime();
-
+	
 	// TODO: create N threads and assign subArrays to the threads so that each thread sorts
 	    // its repective subarray. For example,
-		MedianMultiThread[] arraysOfThread = new MedianMultiThread[numOfThread];
-
-		for (int i = 0; i < numOfThread; i++){
-			arraysOfThread[i] = new MedianMultiThread(i*sizeOfSubarray, (i+1)*sizeOfSubarray);
-		}
-
-	//Tip: you can't create big number of threads in the above way. So, create an array list of threads.
-
-	// TODO: start each thread to execute your sorting algorithm defined under the run() method, for example,
-		MedianMultiThread.initializeArray(array);
-		for (int i = 0; i < numOfThread; i++){
-			arraysOfThread[i].start();
-		}
-
-		for (int i = 0; i < numOfThread; i++){
-			arraysOfThread[i].join();
-		}
-
-	// // TODO: use any merge algorithm to merge the sorted subarrays and store it to another array, e.g., sortedFullArray.
-		MedianMultiThread util = new MedianMultiThread(0,0);
-		// util.mergeSort(0,8);
-		int offset = sizeOfSubarray;
-
-		while (offset != 1048576){
-			for (int i = 0; i < 1048576/offset;i+=2){
-				util.merge(i*offset, (i+1)*offset, (i+2)*offset);
-			}
-			offset*=2;
-		}
-
-
-
-
-
-
+	
+	MedianMultiThread thread1 = new MedianMultiThread(subArray1);
+	MedianMultiThread threadn = new MedianMultiThread(subArrayn);
+	//Tip: you can't create big number of threads in the above way. So, create an array list of threads. 
+	
+	// TODO: start each thread to execute your sorting algorithm defined under the run() method, for example, 
+	thread1.start(); //start thread1 on from run() function
+	threadn.start();//start thread2 on from run() function
+	
+	thread1.join();//wait until thread1 terminates
+	threadn.join();//wait until threadn terminates
+	
+	// TODO: use any merge algorithm to merge the sorted subarrays and store it to another array, e.g., sortedFullArray. 
+		
 	//TODO: get median from sortedFullArray
-		array = Arrays.copyOfRange(util.getInternal(),0,1048576);
-
-		int median = (array[524287] + array[524288])/2;
-
-	// TODO: stop recording time and compute the elapsed time
-
-		long finish = System.nanoTime();
-
+	
+	    //e.g, computeMedian(sortedFullArray);
+	
+	// TODO: stop recording time and compute the elapsed time 
 
 	// TODO: printout the final sorted array
-		for (int i = 0; i < 262144;i++){
-			// System.out.println("" + array[4*i] + " " + array[4*i + 1] + " " + array[4*i+2] + " " + array[4*i+3]);
-		}
-		// for (int i = 0; i < 8;i+=4){
-		// 	System.out.println("" + testArray[i] + " " + testArray[i+1] + " " + testArray[i+2] + " " + testArray[i+3]);
-		// }
-
-		long runningTime = finish-start;
-
+	
 	// TODO: printout median
-	System.out.println("The Median value is: " + median);
-	System.out.println("Running time is " + runningTime/1000000 + " milliseconds\n");
-
+	System.out.println("The Median value is ...");
+	System.out.println("Running time is " + runningTime + " milliseconds\n");
+	
 	}
 
-	// public static double computeMedian(ArrayList<Integer> inputArray) {
-	//   //TODO: implement your function that computes median of values of an array
-	// }
-
+	public static double computeMedian(ArrayList<Integer> inputArray) {
+	  //TODO: implement your function that computes median of values of an array 
+	}
+	
 }
 
 // extend Thread
 class MedianMultiThread extends Thread {
-	private static int[] array;
-	private int low;
-	private int high;
+	private ArrayList<Integer> list;
 
-	public static void initializeArray(int[] newArray){
-		array = newArray;
+	public ArrayList<Integer> getInternal() {
+		return list;
 	}
 
-	public int[] getInternal() {
-		return array;
-	}
-
-	MedianMultiThread(int low, int high) {
-		this.low = low;
-		this.high = high;
+	MedianMultiThread(ArrayList<Integer> array) {
+		list = array;
 	}
 
 	public void run() {
 		// called by object.start()
-		mergeSort(low,high);
-
+		mergeSort(list);
+		
 	}
-
-	public void merge(int low, int mid, int high) {
-		// System.out.println("" + low + " " + mid + " " + high);
-		int[] left = Arrays.copyOfRange(array, low, mid);
-		int[] right = Arrays.copyOfRange(array, mid, high);
-		int arrayPointer = low;
-		int leftCount = 0;
-		int rightCount = 0;
-
-		while (leftCount < left.length && rightCount < right.length){
-			if (left[leftCount] <= right[rightCount]){
-				array[arrayPointer] = left[leftCount];
-				leftCount++;
-			} else {
-				array[arrayPointer] = right[rightCount];
-				rightCount++;
-
-			}
-			arrayPointer++;
-		}
-
-		while (leftCount < left.length){
-			array[arrayPointer] = left[leftCount];
-			leftCount++;
-			arrayPointer++;
-		}
-
-		while (rightCount < right.length){
-			array[arrayPointer] = right[rightCount];
-			rightCount++;
-			arrayPointer++;
-		}
-		return;
-	}
-
+	
 	// TODO: implement merge sort here, recursive algorithm
-	public void mergeSort(int low, int high) {
-		if((high-low)<= 1){
-			return;
-		} else {
-			int mid = (low + high)/2;
-			mergeSort(low, mid);
-			mergeSort(mid, high);
-			merge(low, mid, high);
-			return;
+		public void mergeSort(ArrayList<Integer> array) {
+			
 		}
-	}
 }
