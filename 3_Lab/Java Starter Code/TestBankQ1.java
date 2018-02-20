7bf23fb3ca34d62ac90fe7bee40e9ffed626b515
vincent <Vincent@mymail.sutd.edu.sg>
2018-02-21 04:38:16 +0800

    removed c starter code from lab3

--- 3_Lab/Java Starter Code/TestBankQ1.java
+++
@@ -1,9 +0,0 @@
-
-public class TestBankQ1 {
-
-	public static void main(String[] args) {
-		String [] bankArgs = {"q1.txt"};
-		Banker.main(bankArgs);
-	}
-
-}
--- 3_Lab/Java Starter Code/TestBankQ2.java
+++
@@ -1,9 +0,0 @@
-
-public class TestBankQ2 {
-
-	public static void main(String[] args) {
-		String [] bankArgs = {"q2.txt"};
-		Banker.main(bankArgs);
-	}
-
-}
